54063ca42bb5c061d78ae0174a5b90d42a5792fb
=Zenklot <zenklot11@gmail.com>
2022-01-28 09:45:26 +0700

    soal no 1

---
+++ 1_array_loop.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$aplikasi[1] = 'gtAkademik';
+$aplikasi[2] = 'gtFinansi';
+$aplikasi[3] = 'gtPerizinan';
+$aplikasi[4] = 'gtCampuz';
+$aplikasi[5] = 'gtOviz';
+
+$i = 1;
+while ($i <= 5) {
+    echo $aplikasi[$i] ."<br>";
+    $i++;
+}
